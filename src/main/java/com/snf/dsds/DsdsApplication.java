f17a5b6c3dfe02e6ca9ae7064f7beadeb30bfb83
YJay.Zhou <48643709+zyj12345616@users.noreply.github.com>
2022-01-28 14:11:18 +0800

    优化数据库配置

--- src/main/java/com/snf/dsds/DsdsApplication.java
+++ src/main/java/com/snf/dsds/DsdsApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.snf.dsds.dao")
 @SpringBootApplication
 @ServletComponentScan
+@EnableTransactionManagement
 public class DsdsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
